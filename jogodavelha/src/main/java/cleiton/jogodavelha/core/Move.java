677c1253b3fddd7844bcc50ee28641ab3f3ddef7
Cleiton lima <born4codex@gmail.com>
2020-12-18 21:39:15 -0300

    Mudando visibilidade de atributos e metodos

--- jogodavelha/src/main/java/cleiton/jogodavelha/core/Move.java
+++ jogodavelha/src/main/java/cleiton/jogodavelha/core/Move.java
@@ -2,9 +2,18 @@ package cleiton.jogodavelha.core;
 
 public class Move {
 	
-	int i;
-	int j; // Necessario verificar o pq dessa classes usarem variaveis I e J, pois profissionalmente
+	private int i;
+	private int j; // Necessario verificar o pq dessa classes usarem variaveis I e J, pois profissionalmente
 	       //qual nomenclarutra que é usada;
+	
+	public int getI() {
+		return i;
+		
+	}
+	
+	public int getJ() {
+		return j;
+	}
 	
 
 }
--- jogodavelha/src/main/java/cleiton/jogodavelha/core/Player.java
+++ jogodavelha/src/main/java/cleiton/jogodavelha/core/Player.java
@@ -2,18 +2,32 @@ package cleiton.jogodavelha.core;
 
 public class Player {
 	
-	String name;
-	char symbol;
-	Board board;
+	private String name;
+	private char symbol;
+	private Board board;
 	
-	Move inputMove() {
+	 public Move inputMove() {
 		return null;
 		
 	}
 	
-	void play() {
+	 public void play() {
 		
 		
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public char getSymbol() {
+		return symbol;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+	 
+	 
 
 }
