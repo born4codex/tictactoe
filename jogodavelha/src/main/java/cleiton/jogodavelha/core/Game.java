a58908de02b522e58622cbbef210d10784406049
Cleiton lima <born4codex@gmail.com>
2021-01-04 01:10:33 -0300

    criando os jogadores do jogo

--- jogodavelha/src/main/java/cleiton/jogodavelha/core/Game.java
+++ jogodavelha/src/main/java/cleiton/jogodavelha/core/Game.java
@@ -4,21 +4,29 @@ import cleiton.jogodavelha.Constants;
 import cleiton.jogodavelha.ui.UI;
 
 public class Game {
-	
+
 	Board board = new Board();
 	Player[] players = new Player[Constants.SYMBOL_PLAYERS.length];
-	
+
 	public void play() {
-		
-		
+
 		UI.printGameTitle();
-		
-		
-		board.print();
-		
-		//UI.readInput("Nome do jogador: ");
-	
-	
+
+		for (int i = 0; i < players.length; i++) {
+			player[i] = createPlayer(i);
+
+		}
+
+	}
+
+	private Player createPlayer(int index) {
+		String name = UI.readInput("Jogador " + (index + 1) + "=>"); 
+		char symbol = Constants.SYMBOL_PLAYERS[index]
+		Player player = new Player(name, board, symbol);
+
+		UI.printText("O jogador '" + name + "' vai usar o símbolo '" + symbol + "'");
+
+		return player;
 	}
 
 }
